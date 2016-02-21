46f3bacab2b5972b22bd9f9f1ac716a8ae0f47cc
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-02-21 21:44:50 +0100

    ADD: PumperService Testclass

--- app/src/androidTest/java/ch/chiodo/pumper/persistance/PumperServiceTest.java
+++ app/src/androidTest/java/ch/chiodo/pumper/persistance/PumperServiceTest.java
@@ -1,6 +1,39 @@
 package ch.chiodo.pumper.persistance;
 
-import org.junit.Test;
-public class PumperServiceTest{
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ch.chiodo.pumper.persistence.PumperDbHelper;
+import ch.chiodo.pumper.persistence.PumperService;
+import static org.mockito.Mockito.*;
+
+public class PumperServiceTest extends AndroidTestCase{
+    private SQLiteOpenHelper fakeDbHelper;
+    private PumperService service;
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        fakeDbHelper = new PumperDbHelper(context);
+        service = new PumperService(fakeDbHelper);
+    }
+    @Test
+    public void testGetTraining(){
+        SQLiteDatabase db = fakeDbHelper.getReadableDatabase();
+
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        fakeDbHelper.close();
+        super.tearDown();
+    }
 }
