6dc74bb4bb5abc5cb27c8439cdc9d23842093256
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-02-27 22:55:08 +0100

    MOD: device name column name

--- app/src/main/java/ch/chiodo/pumper/persistence/DeviceContract.java
+++ app/src/main/java/ch/chiodo/pumper/persistence/DeviceContract.java
@@ -11,7 +11,7 @@ public final class DeviceContract {
 
     public static abstract class Device implements BaseColumns {
         public static final String TABLE_NAME = "device";
-        public static final String COLUMN_NAME_DEVICE_ID = "deviceId";
+        public static final String COLUMN_NAME_DEVICE_NAME = "deviceName";
     }
 
 }
