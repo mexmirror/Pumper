570437d701754439d9dbffd7b45c1d420a561ab6
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-03-18 22:34:55 +0100

    ADD: settings fragment

---
+++ app/src/main/java/ch/chiodo/pumper/presentation/view/SettingsFragment.java
@@ -0,0 +1,38 @@
+package ch.chiodo.pumper.presentation.view;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ch.chiodo.pumper.R;
+import ch.chiodo.pumper.presentation.viewmodel.SettingsViewModel;
+
+public class SettingsFragment extends Fragment {
+    private static SettingsViewModel viewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        viewModel = new SettingsViewModel(view.getContext().getApplicationContext());
+        Button saveButton = (Button) view.findViewById(R.id.setting_add_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String trainingName = ((EditText)v.findViewById(R.id.setting_training_name)).getText().toString();
+                try {
+                    viewModel.addTraining(trainingName);
+                } catch (IllegalAccessException e) {
+                    Snackbar.make(v, "Error while adding training", Snackbar.LENGTH_LONG);
+                }
+            }
+        });
+        return view;
+    }
+
+}
