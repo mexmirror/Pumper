f38356c09c1bd5fef6383d75e79e2fe75ca077c3
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-03-18 22:34:45 +0100

    ADD: Constant class

---
+++ app/src/main/java/ch/chiodo/pumper/presentation/Constants.java
@@ -0,0 +1,8 @@
+package ch.chiodo.pumper.presentation;
+
+public class Constants {
+    public static final String TRAINING_ID = "trainingId";
+    public static final String DEVICE_ID = "deviceId";
+    public static final String WEIGHT = "weight";
+    public static final String REPETITION = "repetition";
+}
