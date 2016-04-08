4e76447de449b3bde0080d459221975cebba76bd
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-08 13:58:49 +0200

    ADD: databaseoperationexception

---
+++ app/src/main/java/ch/chiodo/pumper/infrastructure/exception/DatabaseOperationException.java
@@ -0,0 +1,17 @@
+package ch.chiodo.pumper.infrastructure.exception;
+
+public class DatabaseOperationException extends Exception{
+    public DatabaseOperationException(){
+        super();
+    }
+    public DatabaseOperationException(String message){
+        super(message);
+    }
+    public DatabaseOperationException(String message, Throwable cause){
+        super(message, cause);
+    }
+    public DatabaseOperationException(Throwable cause){
+        super(cause);
+    }
+
+}
