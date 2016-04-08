fb9ed6075dd9affe5a54208549230c2e211bd5e5
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-08 14:11:23 +0200

    MOD: set log to logger interface

--- app/src/main/java/ch/chiodo/pumper/presentation/viewmodel/BaseViewModel.java
+++ app/src/main/java/ch/chiodo/pumper/presentation/viewmodel/BaseViewModel.java
@@ -3,13 +3,17 @@ package ch.chiodo.pumper.presentation.viewmodel;
 import android.content.Context;
 
 import ch.chiodo.pumper.PumperApplication;
-import ch.chiodo.pumper.persistence.IPumperService;
+import ch.chiodo.pumper.infrastructure.logging.LogService;
+import ch.chiodo.pumper.infrastructure.logging.Logger;
+import ch.chiodo.pumper.persistence.dataaccess.IPumperService;
 
 public abstract class BaseViewModel {
     protected IPumperService service;
+    protected final Logger log;
 
     public BaseViewModel(Context context){
         service = ((PumperApplication)context.getApplicationContext()).getPumperService();
+        log = LogService.getInstance();
     }
 
 }
