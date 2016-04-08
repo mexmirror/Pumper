ab316a929a69aa800d249e24f61a3432b194c29f
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-08 21:04:39 +0200

    ADD: BaseViewModel

--- app/src/main/java/ch/chiodo/pumper/presentation/viewmodel/BaseViewModel.java
+++ app/src/main/java/ch/chiodo/pumper/presentation/viewmodel/BaseViewModel.java
@@ -5,10 +5,10 @@ import android.content.Context;
 import ch.chiodo.pumper.PumperApplication;
 import ch.chiodo.pumper.infrastructure.logging.LogService;
 import ch.chiodo.pumper.infrastructure.logging.Logger;
-import ch.chiodo.pumper.persistence.dataaccess.IPumperService;
+import ch.chiodo.pumper.persistence.dataaccess.PumperService;
 
 public abstract class BaseViewModel {
-    protected IPumperService service;
+    protected PumperService service;
     protected final Logger log;
 
     public BaseViewModel(Context context){
