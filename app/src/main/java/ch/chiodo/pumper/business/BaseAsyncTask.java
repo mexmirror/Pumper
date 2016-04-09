294d92fd0312d92807aaf661ea14b806d815e499
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-09 19:01:32 +0200

    MOD: overwrite onpostexecute in baseasynctask

--- app/src/main/java/ch/chiodo/pumper/business/BaseAsyncTask.java
+++ app/src/main/java/ch/chiodo/pumper/business/BaseAsyncTask.java
@@ -6,8 +6,20 @@ import ch.chiodo.pumper.persistence.dataaccess.PumperService;
 
 public abstract class BaseAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result>{
     protected PumperService service;
+    protected Callback<Result> callback;
 
-    public BaseAsyncTask(PumperService service){
+    public BaseAsyncTask(PumperService service, Callback<Result> callback){
         this.service = service;
+        this.callback = callback;
+    }
+
+    @Override
+    protected void onPostExecute(Result result) {
+        super.onPostExecute(result);
+        try{
+            callback.onSuccess(result);
+        } catch (Exception e){
+            callback.onError(e);
+        }
     }
 }
