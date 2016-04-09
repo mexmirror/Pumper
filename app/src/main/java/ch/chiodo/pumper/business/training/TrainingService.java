ed0ff4b11b423eba21b2fbfaf5e373f45a818ec0
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-09 19:18:05 +0200

    MOD: add businessservice

--- app/src/main/java/ch/chiodo/pumper/business/training/TrainingService.java
+++ app/src/main/java/ch/chiodo/pumper/business/training/TrainingService.java
@@ -8,11 +8,12 @@ import android.os.IBinder;
 import java.util.List;
 
 import ch.chiodo.pumper.PumperApplication;
+import ch.chiodo.pumper.business.BusinessService;
 import ch.chiodo.pumper.business.Callback;
 import ch.chiodo.pumper.model.Training;
 import ch.chiodo.pumper.persistence.dataaccess.PumperService;
 
-public class TrainingService extends Service{
+public class TrainingService extends Service implements BusinessService<Training>{
     private IBinder binder = new TrainingBinder();
     private PumperService service;
 
@@ -22,27 +23,27 @@ public class TrainingService extends Service{
         return binder;
     }
 
-    public void getTraining(long id, Callback<Training> callback){
+    public void get(long id, Callback<Training> callback){
         GetTrainingTask task = new GetTrainingTask(service, callback);
         task.execute(id);
     }
 
-    public void insertTraining(Training training, Callback<Training> callback){
+    public void insert(Training training, Callback<Training> callback){
         InsertTrainingTask task = new InsertTrainingTask(service, callback);
         task.execute(training);
     }
 
-    public void updateTraining(Training training, Callback<Training> callback){
+    public void update(Training training, Callback<Training> callback){
         UpdateTrainingTask task = new UpdateTrainingTask(service, callback);
         task.execute(training);
     }
 
-    public void deleteTraining(Training training, Callback<Training> callback){
+    public void delete(Training training, Callback<Training> callback){
         DeleteTrainingTask task = new DeleteTrainingTask(service, callback);
         task.execute(training);
     }
 
-    public void getTrainingList(Callback<List<Training>> callback){
+    public void getList(Callback<List<Training>> callback){
         GetTrainingListTask task = new GetTrainingListTask(service, callback);
         task.execute();
     }
